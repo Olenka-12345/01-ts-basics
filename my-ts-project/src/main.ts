173d12d1d16d18368b8cf5b6df15dba3e3743c1c
Lena <lena2316@ukr.net>
2025-08-24 23:37:00 +0200

    dfff

--- my-ts-project/src/main.ts
+++
@@ -1,8 +0,0 @@
-import '/src/task-1';
-import '/src/task-2';
-import '/src/task-3';
-import '/src/task-4';
-import './src/task-5';
-import './src/task-6';
-import './src/task-7';
-import './src/task-8';
