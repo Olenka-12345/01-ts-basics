72fcc7700868287a981395dcdd1bd6073909a8cd
Lena <lena2316@ukr.net>
2025-08-27 13:10:37 +0200

    fggh

--- src/task-2.ts
+++ src/task-2.ts
@@ -1,15 +1,14 @@
-
-interface product {
+interface Product {
   readonly id: number;
   title: string;
   description?: string;
 }
 
-
-const product = {
+const product: Product = {
   id: 1,
   title: "Tablet",
   description: "Compact and fast",
 };
 
-console.log(`Product: ${JSON.stringify(product)}`);
+
+
